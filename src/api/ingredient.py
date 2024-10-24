fc4d06fd0c78a7aa3965d38312525bc3fea2e83d
sethlangel <seth@blueplate.ai>
2024-10-23 20:27:38 -0700

    - Basic skeleton for Ingredients endpoints.

--- src/api/ingredient.py
+++ src/api/ingredient.py
@@ -8,10 +8,39 @@ router = APIRouter(
     tags=["Ingredient"],
 )
 
-@router.post("/create")
-def ingredient_create():
+class Ingredient(BaseModel):
+    name: str
+    serving_size_type: str
+    serving_size: int
+    calories: int
+
+@router.post("/")
+def ingredient_create(ingredient: Ingredient):
     
     # with db.engine.begin() as connection:
     #     connection.execute(sqlalchemy.text("""INSERT INTO ingredient VALUES ()"""))
 
+    #TODO: Once DB is setup change this to return ID
+    return ingredient
+
+@router.put("/{ingredient_id}")
+def ingredient_update(ingredient: Ingredient):
+
+    return ingredient
+
+@router.delete("{ingredient_id}")
+def ingredient_delete(ingredient: Ingredient):
+
     return "OK"
+
+@router.get("/{ingredient_id}")
+def get_ingredient(ingredient_id: int):
+    ingredient = {}
+
+    return ingredient
+
+@router.get("/")
+def get_ingredients():
+    ingredients = [{}]
+
+    return ingredients
