cdbbcb13ec51e92164773df2dabd2379dbd94bb6
Connor S <ohvconnor@gmail.com>
2024-10-25 20:10:18 -0700

    Added recipe creation

--- src/api/recipe.py
+++ src/api/recipe.py
@@ -1,17 +1,31 @@
-from fastapi import APIRouter, Depends, Request
+from typing import Optional
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+
 import sqlalchemy
 from src import database as db
 
+
 router = APIRouter(
-    prefix="/recipe",
+    prefix="/recipes",
     tags=["Recipe"],
 )
 
-# @router.post("/create")
-# def user_create():
-    
-    # with db.engine.begin() as connection:
-        
 
-    # return "OK"
+class Recipe(BaseModel):
+    author_id: Optional[int] = None
+    name: Optional[str] = None
+    servings: Optional[int] = None
+
+
+@router.post("/create")
+def create_recipe(recipe: Recipe):
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(
+            "INSERT INTO recipes (author_id, name, servings) VALUES (:id, :name, :servings) RETURNING id"),
+            { "id": recipe.author_id, "name": recipe.name, "servings": recipe.servings }
+        )
+
+        return result.mappings().one()  
+
+    raise HTTPException(status_code=400, detail="Failed to create user.")
