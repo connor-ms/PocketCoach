2d07a893a7679e9ed79223de4f0173512a7a67bd
sethlangel <seth@blueplate.ai>
2024-11-22 14:25:55 -0800

    V2 Redo

--- src/api/ingredient.py
+++ src/api/ingredient.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, Response
 from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
@@ -12,38 +12,88 @@ router = APIRouter(
 )
 
 class Ingredient(BaseModel):
-    name: str
-    serving_size_type: str
+    description: str
+    serving_size_unit: str
     serving_size: int
     calories: int
 
+def validate_range(value: int, min: int, max: int, attribute: str):
+    if value < min:
+        raise Exception(f"{attribute} is invalid, must be {min} or above.")
+    elif value >= max:
+        raise Exception(f"{attribute} is invalid, must be {max} or below.")
+
+def validate_info(ingredient: Ingredient) -> bool:
+    validate_range(ingredient.calories, 0, 2000, "Calories")
+    validate_range(ingredient.serving_size, 1, 50, "Serving size")
+
 @router.post("/")
-def ingredient_create(ingredient: Ingredient):
+def create_ingredient(ingredient: Ingredient):
+    try:
+        validate_info(ingredient)
+
+        ingredient_data = ingredient.dict()
+
+        with db.engine.begin() as connection:
+            stmt = sqlalchemy.insert(db.ingredients).values(ingredient_data)
+            result = connection.execute(stmt.returning(db.ingredients.c.fdc_id))
+            return result.mappings().one()
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Invalid input: {e}")
     
-    # with db.engine.begin() as connection:
-    #     connection.execute(sqlalchemy.text("""INSERT INTO ingredient VALUES ()"""))
-
-    #TODO: Once DB is setup change this to return ID
-    return ingredient
-
 @router.put("/{ingredient_id}")
-def ingredient_update(ingredient: Ingredient):
+def update_ingredient(ingredient_id, ingredient: Ingredient):
+    try:
+        validate_info(ingredient)
 
-    return ingredient
+        ingredient_data = ingredient.dict()
 
+        with db.engine.begin() as connection:
+            stmt = sqlalchemy.update(db.ingredients).values(ingredient_data).where(db.ingredients.c.fdc_id == ingredient_id)
+            connection.execute(stmt)
+            
+            return Response(status_code=200)
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Invalid input: {e}")
+    
 @router.delete("{ingredient_id}")
-def ingredient_delete(ingredient: Ingredient):
-
-    return "OK"
-
-@router.get("/{ingredient_id}")
+def delete_ingredient(ingredient_id):
+    try:
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text("""DELETE FROM usda_branded WHERE fdc_id = :id"""), {"id": ingredient_id})
+            return Response(status_code=200)
+        
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid ingredient id. Please try and enter another id.")
+    
+@router.get("/id/{ingredient_id}")
 def get_ingredient(ingredient_id: int):
-    with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM usda_branded WHERE fdc_id = :id"), {"id": ingredient_id})
-        return result.mappings().first()
+    try:
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text("SELECT * FROM usda_branded WHERE fdc_id = :id"), {"id": ingredient_id})
+            value = result.mappings().one_or_none()
+        
+            if value is None:
+                raise Exception("Incorrect ingredient id.")
 
-@router.get("/")
-def get_ingredients():
-    ingredients = [{}]
+            return value
+        
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Invalid input: {e}")
+    
+@router.get("/name/{ingredient_name}")
+def get_ingredients_by_name(ingredient_name: str, page: int):
+    try:
+        if page >= 0:
+            offset = page * 10
+        else:
+            raise Exception("Page number needs to be greater than or equal to 0.")
+        
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text("""SELECT * FROM usda_branded WHERE description ILIKE :name LIMIT 10 OFFSET :offset"""), {"name": ingredient_name, "offset": offset})
 
-    return ingredients
+            return result.mappings().all()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Invalid Inputs: {e}")
